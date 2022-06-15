531b3f4f069d8a6611da0a53d69011e49d978cf0
Borg <xiaoborg@gmail.com>
2022-06-16 00:10:04 +0800

    version 1.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -31,42 +31,27 @@ const replace = require('gulp-replace')
 // 编译时间
 const udpateTime = moment().format('YYYY/MM/DD HH:MM:SS')
 
+// 获取当前分支
 const branch = childProcess
   .execSync('git rev-parse --abbrev-ref HEAD')
   .toString()
   .replace(/\s+/, '')
 // 获取命令行参数，这里主要用来判断是开发状态还是产品状态
 let PRODUCTION = !!yargs.argv.production
-console.log(PRODUCTION)
 const currentVersion = PRODUCTION ? process.env.npm_config_currentVersion : ''
 // Load settings from config.yml
-const { COMPATIBILITY, PORT, UNCSS_OPTIONS, PATHS } = loadConfig()
-
+const YmlConfig = loadConfig()
 showConsole('*', 'currentEnv', PRODUCTION ? 'production' : 'devlopment')
-
 showConsole('*', 'currentVersion', currentVersion)
-
 showConsole('*', 'StartTime', udpateTime)
-// if(currentVersion === undefined) {
-//   PRODUCTION = false
-// }
-const cssPath = [
-  {
-    src: './src/assets/scss/app.scss', // need complile css
-    dist: {
-      develop: '/assets/css/develop',
-      production: '/assets/css/production'
-    }
-  }
-]
-const cssWatchPath = ['./src/assets/scss/*.scss', './src/assets/scss/**/*.scss']
 
+//加载配置文件
 function loadConfig() {
   let ymlFile = fs.readFileSync('config.yml', 'utf8')
   return yaml.load(ymlFile)
 }
 
-// 把html文件放入dist
+// html -> dist
 function pages() {
   return (
     gulp
@@ -74,12 +59,13 @@ function pages() {
       // .pipe(gulp.dest(PATHS.dist + '/pages'));
       .pipe(fileinclude())
       .pipe(beautify.html({ indent_size: 2 }))
-      .pipe(gulp.dest(PATHS.dist))
+      .pipe(gulp.dest(YmlConfig.PATHS.dist))
   )
 }
+// 将dist中html 变量替换
 function pagesDist() {
   return gulp
-    .src(['dist/index.html', 'dist/**/*.html', 'dist/**/**/*.html'])
+    .src(YmlConfig.PATHS.html.replace)
     .pipe(
       replace(
         '@currentEnvironment',
@@ -87,13 +73,15 @@ function pagesDist() {
       )
     )
     .pipe(beautify.html({ indent_size: 2 }))
-    .pipe(gulp.dest(PATHS.dist))
+    .pipe(gulp.dest(YmlConfig.PATHS.dist))
 }
 
 // Copy files out of the assets folder
 // This task skips over the "img", "js", and "scss" folders, which are parsed separately
 function copy() {
-  return gulp.src(PATHS.assets).pipe(gulp.dest(PATHS.dist + '/assets'))
+  return gulp
+    .src(YmlConfig.PATHS.assets)
+    .pipe(gulp.dest(YmlConfig.PATHS.dist + '/assets'))
 }
 
 // 启动本地服务器和浏览器
@@ -101,7 +89,7 @@ function server(done) {
   browserSync.init(
     {
       server: './dist',
-      port: PORT
+      port: YmlConfig.PORT
     },
     done
   )
@@ -134,7 +122,7 @@ let webpackConfig = {
 function js() {
   return (
     gulp
-      .src(PATHS.entries)
+      .src(YmlConfig.PATHS.js.entries)
       .pipe(named())
       .pipe(sourcemaps.init())
       .pipe(webpackStream(webpackConfig, webpack2))
@@ -149,12 +137,12 @@ function js() {
       )
       .pipe(gulpif(!(PRODUCTION && branch === 'master'), sourcemaps.write()))
       .pipe(header('/*' + udpateTime + '*/\n'))
-      .pipe(rename({ suffix: '.v3' }))
+
       .pipe(
         gulp.dest(
           PRODUCTION && branch === 'master'
-            ? PATHS.dist + '/assets/js/production'
-            : PATHS.dist + '/assets/js/develop'
+            ? YmlConfig.PATHS.js.dist.production
+            : YmlConfig.PATHS.js.dist.develop
         )
       )
   )
@@ -163,45 +151,46 @@ function js() {
 //sass
 function css() {
   const postCssPlugins = [
-    autoprefixer({ overrideBrowserslist: COMPATIBILITY }),
+    autoprefixer({ overrideBrowserslist: YmlConfig.COMPATIBILITY }),
     PRODUCTION && branch === 'master' ? postcssImport : '',
     PRODUCTION && branch === 'master' ? cssMqpacker : cssMqpacker,
     PRODUCTION && branch === 'master' ? cssnano : ''
   ].filter(Boolean)
   if (PRODUCTION && branch === 'master') {
-    return gulp
-      .src(cssPath[0].src)
-      .pipe(sourcemaps.init())
-      .pipe(sass({ includePaths: PATHS.sass }).on('error', sass.logError))
-      .pipe(postcss(postCssPlugins))
-      .pipe(cssmin())
-      .pipe(sourcemaps.write())
-      .pipe(header('/*' + udpateTime + '*/\n'))
-      .pipe(rename({ suffix: '.v3' }))
-      .pipe(gulp.dest(PATHS.dist + cssPath[0].dist.production))
-      .pipe(browserSync.reload({ stream: true }))
-  } else {
     return (
       gulp
-        .src(cssPath[0].src)
+        // .src(cssPath[0].src)
+        // .src(YmlConfig.PATHS.css.entries)
+        .src(YmlConfig.PATHS.css.entries)
         .pipe(sourcemaps.init())
-        .pipe(sass({ includePaths: PATHS.sass }).on('error', sass.logError))
+        .pipe(sass().on('error', sass.logError))
         .pipe(postcss(postCssPlugins))
-        // .pipe(sourcemaps.write())
-        .pipe(header('/*' + udpateTime + '*/'))
-        .pipe(rename({ suffix: '.v3' }))
-        .pipe(gulp.dest(PATHS.dist + cssPath[0].dist.develop))
+        .pipe(cssmin())
+        .pipe(sourcemaps.write())
+        .pipe(header('/*' + udpateTime + '*/\n'))
+        .pipe(gulp.dest(YmlConfig.PATHS.css.dist.production))
         .pipe(browserSync.reload({ stream: true }))
     )
+  } else {
+    return gulp
+      .src(YmlConfig.PATHS.css.entries)
+      .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(postcss(postCssPlugins))
+      .pipe(sourcemaps.write())
+      .pipe(header('/*' + udpateTime + '*/'))
+      .pipe(gulp.dest(YmlConfig.PATHS.css.dist.develop))
+      .pipe(browserSync.reload({ stream: true }))
   }
 }
 
 // Copy images to the "dist" folder
 function images() {
   return gulp
-    .src('src/assets/img/**/*')
-    .pipe(gulp.dest(PATHS.dist + '/assets/img'))
+    .src(YmlConfig.PATHS.img.dev)
+    .pipe(gulp.dest(YmlConfig.PATHS.img.pro))
 }
+
 async function cleanDist() {
   del.sync(['dist'])
 }
@@ -210,14 +199,16 @@ async function cleanDist() {
 function watch() {
   // gulp.watch(PATHS.assets, copy);
   gulp
-    .watch('./src/pages/**/*.html')
+    .watch(YmlConfig.PATHS.watch.html)
     .on('all', gulp.series(pages, pagesDist, browserSync.reload))
-  gulp.watch(cssWatchPath).on('all', gulp.series(css, browserSync.reload))
   gulp
-    .watch('./src/assets/js/*.js')
+    .watch(YmlConfig.PATHS.watch.css)
+    .on('all', gulp.series(css, browserSync.reload))
+  gulp
+    .watch(YmlConfig.PATHS.watch.js)
     .on('all', gulp.series(js, browserSync.reload))
   gulp
-    .watch('./src/assets/img/**/*')
+    .watch(YmlConfig.PATHS.watch.img)
     .on('all', gulp.series(images, browserSync.reload))
 }
 
